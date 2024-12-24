f8a4bf03640540682e1d1030118c6f393e77c07b
21120455 <hoai0455fit@gmail.com>
2024-12-24 19:12:56 +0700

    refactor: add commend for controller

--- src/main/java/com/vou/backend/voucher/controller/VoucherController.java
+++ src/main/java/com/vou/backend/voucher/controller/VoucherController.java
@@ -21,35 +21,83 @@ import java.util.List;
 @RequestMapping("/vouchers")
 public class VoucherController {
     private final VoucherService voucherService;
+    /**
+     * Create a new voucher.
+     *
+     * @param voucherDTO the VoucherDto containing the details of the voucher
+     * @return the created VoucherResponseDto
+     * @throws ExistedVoucherException if the voucher already exists
+     */
     @PostMapping
     public ResponseEntity<?> createVoucher(@Valid @RequestBody VoucherDto voucherDTO) throws ExistedVoucherException {
         VoucherResponseDto voucher = voucherService.createVoucher(voucherDTO);
         return new ResponseEntity<>(voucher, HttpStatus.CREATED);
     }
+    /**
+     * Get all vouchers.
+     *
+     * @param pageable the pageable information
+     * @return a list of VoucherResponseDto
+     */
     @GetMapping
     public ResponseEntity<?> getAllVouchers(Pageable pageable) {
         Page<VoucherResponseDto> voucherList = voucherService.getAllVouchers(pageable);
         return new ResponseEntity<>(voucherList,HttpStatus.OK);
     }
+    /**
+     * Get a voucher by its ID.
+     *
+     * @param id the ID of the voucher
+     * @return the VoucherResponseDto
+     * @throws VoucherNotFoundException if the voucher is not found
+     */
     @GetMapping("/{id}")
     public ResponseEntity<?> getAVoucher(@PathVariable String id) throws VoucherNotFoundException {
         VoucherResponseDto voucher = voucherService.getVoucher(id);
         return new ResponseEntity<>(voucher,HttpStatus.OK);
     }
+
+    /**
+     * Update a voucher by its ID.
+     *
+     * @param id the ID of the voucher
+     * @param voucherDto the UpdateVoucherDto containing the details of the voucher
+     * @return the updated VoucherResponseDto
+     * @throws VoucherNotFoundException if the voucher is not found
+     */
     @PutMapping("/{id}")
     public ResponseEntity<?>  updateAVoucher(@PathVariable String id,@Valid @RequestBody UpdateVoucherDto voucherDto) throws VoucherNotFoundException {
         VoucherResponseDto voucher = voucherService.updateVoucher(id,voucherDto);
         return new ResponseEntity<>(voucher,HttpStatus.OK);
     }
+    /**
+     * Delete a voucher by its ID.
+     *
+     * @param id the ID of the voucher
+     * @return a ResponseEntity with no content
+     * @throws VoucherNotFoundException if the voucher is not found
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteVoucher(@PathVariable String id) throws VoucherNotFoundException {
         voucherService.deleteVoucherById(id);
         return ResponseEntity.noContent().build();
     }
+    /**
+     * Get all vouchers of a user by its ID.
+     *
+     * @param userId the ID of the user
+     * @return a list of VoucherResponseDto
+     */
     @GetMapping("/user/{userId}")
     public ResponseEntity<?> getUserVouchers(@PathVariable Long userId) {
         return new ResponseEntity<>(voucherService.getVouchersByUserId(userId),HttpStatus.OK);
     }
+    /**
+     * Get all vouchers of a campaign by its ID.
+     *
+     * @param campaignId the ID of the campaign
+     * @return a list of VoucherResponseDto
+     */
     @GetMapping("/campaign/{campaignId}")
     public  ResponseEntity<?> getCampaignVouchers(@PathVariable Long campaignId) {
         return new ResponseEntity<>(voucherService.getVouchersByCampaignId(campaignId),HttpStatus.OK);
