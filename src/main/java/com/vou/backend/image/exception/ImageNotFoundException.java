8206aaa6e45ccb40c03d31439fa0a564d8620f16
hoangkhoachau <21120481@student.hcmus.edu.vn>
2024-12-26 13:18:15 +0700

    add mongodb image service api

---
+++ src/main/java/com/vou/backend/image/exception/ImageNotFoundException.java
@@ -0,0 +1,7 @@
+package com.vou.backend.image.exception;
+
+public class ImageNotFoundException extends Exception {
+    public ImageNotFoundException(String message) {
+        super(message);
+    }
+}
