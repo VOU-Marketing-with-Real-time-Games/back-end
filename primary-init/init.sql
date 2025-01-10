51120e7e6d0752f3b2f9cb59ff7c72f1f8b28d92
To Phuong Hieu <Phuonghieuto@gmail.com>
2025-01-10 16:30:17 +0700

    Revert "V2"

--- primary-init/init.sql
+++
@@ -1,8 +0,0 @@
--- Create the replication user with the correct syntax
-CREATE USER 'replica_user'@'%' IDENTIFIED BY 'replica_pass';
-
--- Grant replication privileges
-GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
-
--- Apply the changes
-FLUSH PRIVILEGES;
